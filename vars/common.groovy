8b7dc79b1a7c23a23eb0eb7f35760c1d4da70cbd
metal <metalheadkrn@gmail.com>
2026-05-10 10:58:10 +0530

    Adding steps into library

---
+++ vars/common.groovy
@@ -0,0 +1,42 @@
+def unitTests() {
+    stage('Unit Tests'){
+        echo 'OK'
+    }
+}
+
+def integrationTests() {
+    stage('Integration Tests'){
+        echo 'OK'
+        sh 'env'
+    }
+}
+
+def codeQuality() {
+    stage('Code Quality'){
+        echo 'OK'
+    }
+}
+
+def sast() {
+    stage('SAST'){
+        echo 'OK'
+    }
+}
+
+def sca() {
+    stage('SCA'){
+        echo 'OK'
+    }
+}
+
+def secretDetection() {
+    stage('SECRET Detection'){
+        echo 'OK'
+    }
+}
+
+def artifactProduce() {
+    stage('Artifact Produce'){
+        echo 'OK'
+    }
+}
