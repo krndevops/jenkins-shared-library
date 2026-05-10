f88ab99df4a2e42dc4e24cb77117051b9c0a4c60
metal <metalheadkrn@gmail.com>
2026-05-10 11:16:29 +0530

    Moving pipeline to library

---
+++ vars/mainCI.groovy
@@ -0,0 +1,21 @@
+def call() {
+
+    node('workstation') {
+        if (env.BRANCH_NAME == 'main') {
+            echo 'Nothing to Do'
+
+        }
+        else if (env.TAG_NAME =~ '.*') {
+            common.sast()
+            common.sca()
+            common.secretDetection()
+            common.artifactProduce()
+        }
+        else if (env.BRANCH_NAME =~ '.*') {
+            common.unitTests()
+            common.integrationTests()
+            common.codeQuality()
+        }
+    }
+
+}
